92f2c68fa5f99e0b18bb75eb61cf5af635d2c1d6
Dan Scott <tehdanscott@gmail.com>
2021-06-29 19:49:06 +0100

    updated media queries

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import River from './components/River/River';
 import StatsContainer from './components/StatsContainer/StatsContainer';
 import Jungle from './components/Jungle/Jungle';
@@ -5,16 +6,18 @@ import Jungle from './components/Jungle/Jungle';
 const completed = () =>  (state.walked / state.amazonRiverLength) * 100;
 
 const state = {
-  amazonRiverLength: 4000, 
+  amazonRiverLength: 6400, 
   walked: 4000,
 };
 
 const App = () => {
   return (
-    <div className="App">
-      <River complete={completed()} />
-      <Jungle />
+    <div>
       <StatsContainer state={state} completed={completed().toFixed(2)} />
+      <div className="river-and-jungle">
+        <River complete={completed()} />
+        <Jungle />
+      </div>
     </div>
   );
 }
--- src/components/StatsContainer/StatsContainer.js
+++ src/components/StatsContainer/StatsContainer.js
@@ -5,10 +5,10 @@ const StatsContainer = (props) => {
     return (
     <div className="stats-container">
         <div className="text-center font-bold">
-            <h1>Mangotsfield Primary are participating in a sponsored walk to help preserve the Amazon Rainforest</h1>
-            <h2>Mangotsfield Primary School has completed&nbsp;
-                <span className="td-underline">{props.state.walked} miles</span> of&nbsp;
-                <span className="td-underline">{props.state.amazonRiverLength} miles</span> ({props.completed}%)</h2>
+            <h1>Year 5 Students of Mangotsfield Primary are participating in a sponsored walk to help preserve the Amazon Rainforest</h1>
+            <h2>Distance covered&nbsp;
+                <span className="td-underline">{props.state.walked}km</span> of&nbsp;
+                <span className="td-underline">{props.state.amazonRiverLength}km</span> ({props.completed}%)</h2>
             <p>
                 <a href="#">Link to Sponsor Mangotsfield Primary</a>
             </p>
