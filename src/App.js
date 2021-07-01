fd59f2d2fc4159ed6970a37d4ce40357a6501bfe
Dan Scott <464887+CoderDan1@users.noreply.github.com>
2021-07-01 09:47:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Jungle from './components/Jungle/Jungle';
 const completed = () =>  (state.walked / state.amazonRiverLength) * 100;
 
 const state = {
-  amazonRiverLength: 6400, 
-  walked: 2000,
+  amazonRiverLength: 4000, 
+  walked: 348,
 };
 
 const App = () => {
